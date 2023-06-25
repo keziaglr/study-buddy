c97811652c21ccbd5cce050c01d7929bd31271b1
dkhairanb <93834247+dkhairanb@users.noreply.github.com>
2023-06-25 17:50:46 +0700

    Chat Room Added

---
+++ Views/Main/ChatRoomView.swift
@@ -0,0 +1,20 @@
+//
+//  ChatRoomView.swift
+//  studybuddy
+//
+//  Created by Dhil Khairan Badjiser on 25/06/23.
+//
+
+import SwiftUI
+
+struct ChatRoomView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ChatRoomView_Previews: PreviewProvider {
+    static var previews: some View {
+        ChatRoomView()
+    }
+}
