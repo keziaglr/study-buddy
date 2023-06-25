3db42831f7d2c6901e154e3800f85d039d59a6a6
dkhairanb <93834247+dkhairanb@users.noreply.github.com>
2023-06-25 18:27:07 +0700

    UI Chat Room Done

---
+++ Views/Components/MessageInputComponent.swift
@@ -0,0 +1,85 @@
+//
+//  MessageInputComponent.swift
+//  studybuddy
+//
+//  Created by Dhil Khairan Badjiser on 25/06/23.
+//
+
+import SwiftUI
+
+struct MessageInputComponent: View {
+    
+    @State private var testText: String = ""
+    
+    var body: some View {
+        ZStack {
+            TextField("", text: $testText, axis: .vertical)
+                .keyboardType(.default)
+                .padding(EdgeInsets(top: 10, leading: 20, bottom: 10, trailing: 50))
+                .overlay(
+                    RoundedRectangle(cornerRadius: 40)
+                        .stroke(Color(red: 0.906, green: 0.467, blue: 0.157), lineWidth: 2)
+                )
+                
+                .lineLimit(5)
+            
+            //Send Button
+            HStack {
+                Spacer()
+                Button {
+                    print("tapped")
+                } label: {
+                    ZStack(alignment: .center) {
+                        Circle()
+                            .fill(Color(red: 0.906, green: 0.467, blue: 0.157))
+                            .frame(width: 33, height: 33)
+                        
+                        Image("send_button")
+                            .resizable()
+                            .frame(width: 23, height: 23)
+                    }
+                    .frame(width: 33, height: 33) // Add this line to set the fixed size of the ZStack
+                }
+                .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 5))
+            }
+        }
+    }
+}
+
+struct CustomTextfield: TextFieldStyle {
+    
+    let systemImageString: String
+    
+    // Hidden function to conform to this protocol
+    func _body(configuration: TextField<Self._Label>) -> some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 5.0)
+                .stroke(
+                    LinearGradient(
+                        colors: [
+                            .red,
+                            .blue
+                        ],
+                        startPoint: .leading,
+                        endPoint: .trailing
+                    )
+                )
+                .frame(height: 40)
+            
+            HStack {
+                Image(systemName: systemImageString)
+                // Reference the TextField here
+                configuration
+            }
+            .padding(.leading)
+            .foregroundColor(.gray)
+        }
+    }
+}
+
+struct MessageInputComponent_Previews: PreviewProvider {
+    static var previews: some View {
+        MessageInputComponent()
+    }
+}
+
--- Views/Main/ChatRoomView.swift
+++ Views/Main/ChatRoomView.swift
@@ -9,11 +9,42 @@ import SwiftUI
 
 struct ChatRoomView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack (spacing: 0){
+            
+            Rectangle()
+                .fill(Color(red: 0.439, green: 0.843, blue: 0.984))
+                .frame(height: UIScreen.main.bounds.height * 0.07)
+            
+            //Info
+            ChatRoomInfoComponent()
+            
+            //Study Schedule
+            StudyScheduleComponent()
+            
+            //Chat Window
+            ScrollView(showsIndicators: false){
+                HStack{
+                    MessageBubbleComponent(contentMessage: "Halo Halo Halo Halo Halo Halo Halo Halo Halo Halo Halo Halo Halo Halo Halo Halo Halo Halo Halo Halo Halo Halo Halo Halo Halo Halo Halo Halo Halo Halo Halo Halo ", isCurrentUser: true, userName: "Adriel", messageTime: "09.02 PM")
+                        .padding(EdgeInsets(top: 10, leading: 10, bottom: 0, trailing: 0))
+                    Spacer()
+                }
+            }
+    
+            
+            .padding(EdgeInsets(top: 0, leading: 0, bottom: 10, trailing: 0))
+            
+            //Message Input Field
+            MessageInputComponent()
+                .padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 20))
+            
+            
+        }
+        .ignoresSafeArea(edges: .top)
+        
     }
 }
 
-struct ChatRoomView_Previews: PreviewProvider {
+struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ChatRoomView()
     }
