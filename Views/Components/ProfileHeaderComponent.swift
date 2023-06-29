13b51c7333c9d8b8dd1905fa75b0119662bcca66
Kezia Gloria <86879164+keziaglr@users.noreply.github.com>
2023-06-29 19:59:07 +0700

    Revert "Update profile"

--- Views/Components/ProfileHeaderComponent.swift
+++ Views/Components/ProfileHeaderComponent.swift
@@ -15,8 +15,6 @@ struct ProfileHeaderComponent: View {
     @State private var um = UserViewModel()
     @State private var user: UserModel? = nil
     @State var logout = false
-    @State var showPicker = false
-    @State var vm = ProfileViewModel()
     var body: some View {
         NavigationStack {
             VStack {
@@ -28,36 +26,15 @@ struct ProfileHeaderComponent: View {
                     
                     VStack {
                         //profile image
-                        ZStack {
-                            AsyncImage(url: URL(string: user?.image ?? "")) { image in
-                                image
-                                    .resizable()
-                                    .scaledToFill()
-                                    .frame(width: 85, height: 85)
-                                    .cornerRadius(15)
-                                    .padding(.bottom, 14)
-                            } placeholder: {
-                            Image("user")
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(width: 85, height: 85)
-                                    .cornerRadius(15)
-                                    .padding(.bottom, 14)
-                            }
-                            
-                            Button {
-                                showPicker = true
-                            } label: {
-                                ZStack {
-                                    Circle()
-                                        .fill()
-                                        .foregroundColor(Color("Orange"))
-                                        .frame(width: 25)
-                                    Image(systemName: "pencil")
-                                        .foregroundColor(.white)
-                                }
-                            }.offset(x: 35, y: 30)
-
+                        AsyncImage(url: URL(string: user?.image ?? "")) { image in
+                            image
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 85, height: 85)
+                                .cornerRadius(15)
+                                .padding(.bottom, 14)
+                        } placeholder: {
+                            ProgressView()
                         }
                         
                         //name
@@ -65,7 +42,7 @@ struct ProfileHeaderComponent: View {
                             .fontWeight(.bold)
                             .font(.system(size: 20))
                             .foregroundColor(.white)
-                            .padding(.bottom, 2)
+                            .padding(.bottom, 14)
                         
                         //year joined
                         Text(user?.email ?? "")
@@ -98,27 +75,15 @@ struct ProfileHeaderComponent: View {
                 }
                 .edgesIgnoringSafeArea(.all)
                 Spacer()
-            }
-            .onChange(of: showPicker, perform: { newValue in
-                um.getUser(id: Auth.auth().currentUser?.uid ?? "mxVB7MT39gahu7hQ2ddsSDhOqNl1") { retrievedUser in
+            }.task {
+                um.getUser(id: Auth.auth().currentUser?.uid ?? "") { retrievedUser in
                     self.user = retrievedUser
                 }
-            })
-            .task {
-                um.getUser(id: Auth.auth().currentUser?.uid ?? "mxVB7MT39gahu7hQ2ddsSDhOqNl1") { retrievedUser in
-                    self.user = retrievedUser
-                }
-                
         }
         }.navigationDestination(isPresented: $logout) {
             MasterView()
         }
         .navigationBarBackButtonHidden()
-        .sheet(isPresented: $showPicker) {
-            ImagePicker(show: $showPicker) { url in
-                self.vm.uploadProfilePictureToFirebase(url: url)
-            }
-        }
     }
 }
 
