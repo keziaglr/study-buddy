fdbe0f8c7515589f7cfce5cf0a1e27677c1e4ba8
Eric Prasetya Sentosa <eric.sentosa@binus.ac.id>
2023-07-02 13:42:08 +0700

    disable push notif

--- studybuddy/Config/Apps/studybuddyApp.swift
+++ studybuddy/Config/Apps/studybuddyApp.swift
@@ -10,10 +10,10 @@ import Firebase
 
 @main
 struct studybuddyApp: App {
-    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
-//    init(){
-//        FirebaseApp.configure()
-//    }
+//    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    init(){
+        FirebaseApp.configure()
+    }
     
     var body: some Scene {
         WindowGroup {
@@ -21,108 +21,108 @@ struct studybuddyApp: App {
         }
     }
 }
-
-class AppDelegate: UIResponder, UIApplicationDelegate {
-    let gcmMessageIDKey = "gcm.message_id"
-    
-    //configure notification
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        FirebaseApp.configure()
-        
-        // [START set_messaging_delegate]
-        Messaging.messaging().delegate = self
-        // [END set_messaging_delegate]
-        
-        UNUserNotificationCenter.current().delegate = self
-        
-        let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
-        UNUserNotificationCenter.current().requestAuthorization(
-            options: authOptions,
-            completionHandler: { _, _ in }
-        )
-        
-        application.registerForRemoteNotifications()
-        
-        return true
-    }
-    // [START receive_message]
-    
-    func application(_ application: UIApplication,
-                     didReceiveRemoteNotification userInfo: [AnyHashable: Any]) {
-        
-        if let messageID = userInfo[gcmMessageIDKey] {
-            print("Message ID: \(messageID)")
-        }
-        
-        print(userInfo)
-    }
-    
-    func application(_ application: UIApplication,
-                     didReceiveRemoteNotification userInfo: [AnyHashable: Any]) async
-    -> UIBackgroundFetchResult {
-        if let messageID = userInfo[gcmMessageIDKey] {
-            print("Message ID: \(messageID)")
-        }
-        
-        // Print full message.
-        print(userInfo)
-        
-        return UIBackgroundFetchResult.newData
-    }
-    
-    // [END receive_message]
-    
-    func application(_ application: UIApplication,
-                     didFailToRegisterForRemoteNotificationsWithError error: Error) {
-        print("Unable to register for remote notifications: \(error.localizedDescription)")
-    }
-    func application(_ application: UIApplication,
-                     didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-        print("APNs token retrieved: \(deviceToken)")
-    }
-}
-
-@available(iOS 10, *)
-extension AppDelegate: UNUserNotificationCenterDelegate {
-    // Receive displayed notifications for iOS 10 devices.
-    func userNotificationCenter(_ center: UNUserNotificationCenter,
-                                willPresent notification: UNNotification) async
-    -> UNNotificationPresentationOptions {
-        let userInfo = notification.request.content.userInfo
-        
-        //handle the message
-        if let messageID = userInfo[gcmMessageIDKey] {
-            print("Message ID: \(messageID)")
-        }
-        
-        print(userInfo)
-        
-        return [[.banner, .badge, .sound]]
-    }
-    
-    func userNotificationCenter(_ center: UNUserNotificationCenter,
-                                didReceive response: UNNotificationResponse) async {
-        let userInfo = response.notification.request.content.userInfo
-        
-        if let messageID = userInfo[gcmMessageIDKey] {
-            print("Message ID: \(messageID)")
-        }
-        print(userInfo)
-    }
-}
-
-// [END ios_10_message_handling]
-
-extension AppDelegate: MessagingDelegate {
-    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
-        print("Firebase registration token: \(String(describing: fcmToken))")
-        
-        let dataDict: [String: String] = ["token": fcmToken ?? ""]
-        print(dataDict)
-        NotificationCenter.default.post(
-            name: Notification.Name("FCMToken"),
-            object: nil,
-            userInfo: dataDict
-        )
-    }
-}
+//
+//class AppDelegate: UIResponder, UIApplicationDelegate {
+//    let gcmMessageIDKey = "gcm.message_id"
+//    
+//    //configure notification
+//    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+//        FirebaseApp.configure()
+//        
+//        // [START set_messaging_delegate]
+//        Messaging.messaging().delegate = self
+//        // [END set_messaging_delegate]
+//        
+//        UNUserNotificationCenter.current().delegate = self
+//        
+//        let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+//        UNUserNotificationCenter.current().requestAuthorization(
+//            options: authOptions,
+//            completionHandler: { _, _ in }
+//        )
+//        
+//        application.registerForRemoteNotifications()
+//        
+//        return true
+//    }
+//    // [START receive_message]
+//    
+//    func application(_ application: UIApplication,
+//                     didReceiveRemoteNotification userInfo: [AnyHashable: Any]) {
+//        
+//        if let messageID = userInfo[gcmMessageIDKey] {
+//            print("Message ID: \(messageID)")
+//        }
+//        
+//        print(userInfo)
+//    }
+//    
+//    func application(_ application: UIApplication,
+//                     didReceiveRemoteNotification userInfo: [AnyHashable: Any]) async
+//    -> UIBackgroundFetchResult {
+//        if let messageID = userInfo[gcmMessageIDKey] {
+//            print("Message ID: \(messageID)")
+//        }
+//        
+//        // Print full message.
+//        print(userInfo)
+//        
+//        return UIBackgroundFetchResult.newData
+//    }
+//    
+//    // [END receive_message]
+//    
+//    func application(_ application: UIApplication,
+//                     didFailToRegisterForRemoteNotificationsWithError error: Error) {
+//        print("Unable to register for remote notifications: \(error.localizedDescription)")
+//    }
+//    func application(_ application: UIApplication,
+//                     didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+//        print("APNs token retrieved: \(deviceToken)")
+//    }
+//}
+//
+//@available(iOS 10, *)
+//extension AppDelegate: UNUserNotificationCenterDelegate {
+//    // Receive displayed notifications for iOS 10 devices.
+//    func userNotificationCenter(_ center: UNUserNotificationCenter,
+//                                willPresent notification: UNNotification) async
+//    -> UNNotificationPresentationOptions {
+//        let userInfo = notification.request.content.userInfo
+//        
+//        //handle the message
+//        if let messageID = userInfo[gcmMessageIDKey] {
+//            print("Message ID: \(messageID)")
+//        }
+//        
+//        print(userInfo)
+//        
+//        return [[.banner, .badge, .sound]]
+//    }
+//    
+//    func userNotificationCenter(_ center: UNUserNotificationCenter,
+//                                didReceive response: UNNotificationResponse) async {
+//        let userInfo = response.notification.request.content.userInfo
+//        
+//        if let messageID = userInfo[gcmMessageIDKey] {
+//            print("Message ID: \(messageID)")
+//        }
+//        print(userInfo)
+//    }
+//}
+//
+//// [END ios_10_message_handling]
+//
+//extension AppDelegate: MessagingDelegate {
+//    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+//        print("Firebase registration token: \(String(describing: fcmToken))")
+//        
+//        let dataDict: [String: String] = ["token": fcmToken ?? ""]
+//        print(dataDict)
+//        NotificationCenter.default.post(
+//            name: Notification.Name("FCMToken"),
+//            object: nil,
+//            userInfo: dataDict
+//        )
+//    }
+//}
